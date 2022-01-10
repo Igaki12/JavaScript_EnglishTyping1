34d1313e353dd1c7ccce8c029f5b936bee5d1c50
unknown <igatatsu1997@gmail.com>
2022-01-10 16:29:24 +0900

    AddedKeyDownFunction

--- script.js
+++ script.js
@@ -5,15 +5,21 @@ const textLists = ['Hello World','This is my App','How are you?'];
 const createText = () => {
   const p = document.getElementById('text');
   const rnd = Math.floor(Math.random() * textLists.length);
+  p.textContent = '';
   textLists[rnd].split('').map(value => {
     const span = document.createElement('span');
     span.textContent = value;
     p.appendChild(span);
   })
 };
-createText();
 const keyDown = e => {};
 const rankCheck = rank => {};
 const gameOver = id => {};
 const timer = () => {};
-start.addEventListener('click',() => {});
+start.addEventListener('click',() => {
+  createText();
+  document.addEventListener('keydown',keyDown);
+});
+const keyDown = e =>{
+  console.log(e);
+}
