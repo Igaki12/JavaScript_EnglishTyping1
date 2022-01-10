c7a507b4b564791f90d6b0e8aadf91cd9ecaf5e6
unknown <igatatsu1997@gmail.com>
2022-01-10 17:13:10 +0900

    AddedCharacterBlurDesign

--- script.js
+++ script.js
@@ -2,24 +2,29 @@ const wrap = document.getElementById('wrap');
 const start = document.getElementById('start');
 
 const textLists = ['Hello World','This is my App','How are you?'];
+let checkTexts = [];
 const createText = () => {
   const p = document.getElementById('text');
   const rnd = Math.floor(Math.random() * textLists.length);
   p.textContent = '';
-  textLists[rnd].split('').map(value => {
+  checkTexts = textLists[rnd].split('').map(value => {
     const span = document.createElement('span');
     span.textContent = value;
     p.appendChild(span);
+    return span;
   })
 };
-const keyDown = e => {};
+const keyDown = e =>{
+  if(e.key === checkTexts[0].textContent){
+    checkTexts[0].className = 'add-color';
+    checkTexts.shift();
+    if(!checkTexts.length) createText();
+  }
+}
 const rankCheck = rank => {};
 const gameOver = id => {};
 const timer = () => {};
 start.addEventListener('click',() => {
   createText();
   document.addEventListener('keydown',keyDown);
-});
-const keyDown = e =>{
-  console.log(e);
-}
+})
