c127b9de64616c4265d05dfa015e0d36417cb3d6
unknown <igatatsu1997@gmail.com>
2022-01-11 22:42:38 +0900

    AddedResultConfirm

--- script.js
+++ script.js
@@ -12,18 +12,20 @@ const textLists = [
   'Today is sunny','I love JavaScript!','Good morning',
   'I am Japanese','Let it be','Samurai',
   'Typing Game','Information Technology',
-  'I want to be a programmer','What day is today?',
-  'I want to build a web app','Nice to meet you',
-  'Chrome Firefox Edge Safari','machine learning',
-  'Brendan Eich','John Resig','React Vue Angular',
-  'Netscape Communications','undefined null NaN',
-  'Thank you very much','Google Apple Facebook Amazon',
-  'ECMAScript','console.log','for while if switch',
-  'var let const','Windows Mac Linux iOS Android',
-  'programming'];
+  'I want to be a programmer',];
 next.textContent = `first>>　${textLists[0]}`;
 let checkTexts = [];
 let index = 0;
+const retryCheck = () => {
+  let msg = `【結果】\n 成功タイプ数:${score}　　失敗タイプ数:${miss} \n タイプ成功率:` + (100*score/(score+miss)) + `OK:Retry　　Cancel:Quit`;
+  return msg;
+}
+function gameOver() {
+  const result = confirm(retryCheck());
+  if (result === true) {
+    window.location.reload();
+  }
+}
 const createText = (i) => {
   const text = document.getElementById('text');
   text.textContent = '';
@@ -47,14 +49,18 @@ let miss = 0;
 const keyDown = e =>{
   if(e.key === checkTexts[0].textContent){
     wrap.style.backgroundColor = '#666';
-    checkTexts[0].className = 'add-color';
+    checkTexts[0].style = 'color:#777;';
     checkTexts.shift();
     score++;
     good.textContent = '　　良:' + score;
     if(!checkTexts.length) {
+      if(textLists.length < 2){
+        alert('Finished!');
+        gameOver();
+      }
       index++;
       createText(index);
-      rest.textContent = '　　残り:' + (textLists.length-index) + '文';
+      rest.textContent = '　　残り:' + (textLists.length-index-1) + '文';
     }
   }else if(e.key === 'Shift'){
     wrap.style.backgroundColor = '#666';
@@ -64,27 +70,6 @@ const keyDown = e =>{
     bad.textContent = '　　不可:' + miss;
   }
 }
-const rankCheck = score => {
-  let text = '';
-  if(score < 100){
-    text = '【結果】\n Cランク\n 次ランクまであと' + (100-score) + '文字です。';
-  }else if(score < 200){
-    text = '【結果】\n Bランク\n 次ランクまであと' + (200-score) + '文字です。';
-  }else if(score < 300){
-    text = '【結果】\n Aランク\n 次ランクまであと' + (300-score) + '文字です。';
-
-  }else if(score >= 300){
-    text = '';
-  }
-  return score + "文字打てました！ \n" + text + " \n OK:Retry Cancel:Quit";
-};
-const gameOver = id => {
-  clearInterval(id);
-  const result = confirm(rankCheck(score));
-  if(result === true){
-    window.location.reload();
-  }
-};
 const timer = () => {};
 start.addEventListener('click',() => {
   createText(index);
@@ -92,6 +77,6 @@ start.addEventListener('click',() => {
   flex.style.display = 'flex';
   good.textContent = '　　良:' + score;
   bad.textContent = '　　不可:' + miss;
-  rest.textContent = '　　残り:' + (textLists.length) + '文' ;
+  rest.textContent = '　　残り:' + (textLists.length-1) + '文' ;
   document.addEventListener('keydown',keyDown);
 })
